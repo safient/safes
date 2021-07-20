3405ca414311c09fd33f8b305b407983635e9d42
koushith <koushith97@gmail.com>
2021-07-20 11:23:10 +0530

    adds index file to primitive component directory

---
+++ src/components/primitiveComponents/index.ts
@@ -0,0 +1,6 @@
+export * from './Alerts/alert.comp';
+export * from './Avatar/avatar.comp';
+export * from './Buttons/button.comp';
+export * from './Card/card.comp';
+export * from './Icons/icon.component';
+export * from './Text/text.comp';
