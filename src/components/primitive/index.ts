fb74b7234da8ae8a5b1b037dce592fb3ae1ff358
Chanchal Sadhukhan <cksadhukhan@gmail.com>
2021-08-25 15:28:38 +0530

    updates index.ts in components

--- src/components/primitive/index.ts
+++ src/components/primitive/index.ts
@@ -1,5 +1,9 @@
 export * from './Alerts/alert.component';
 export * from './Avatar/avatar.component';
+
+export * from './box/box.component';
+export * from './box/box.component.props';
+
 export * from './Buttons/button.component';
 export * from './Card/card-base.component';
 export * from './Icon-svg/icon-svg.component';
