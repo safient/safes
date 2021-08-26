3b628eff616c7eeca69820005a24eceec42d05ae
koushith <koushith97@gmail.com>
2021-08-27 00:17:55 +0530

    modifies interface and replaced with enums in props file also addressed all changes

--- src/components/primitive/Icon-svg/icon-svg.component.props.ts
+++ src/components/primitive/Icon-svg/icon-svg.component.props.ts
@@ -1,18 +1,28 @@
-import { Color } from '../../../themes';
-import { Image } from '../../../themes';
+import { Color, Image } from '../../../themes';
 
-export const sizes = {
-  small: 14,
-  medium: 18,
-  large: 32,
-  xLarge: 40,
-};
-type Size = keyof typeof sizes;
-
-interface IconProps {
-  name: Image;
-  size?: Size;
-  color?: Color;
+/**
+ * Different Sizes that are supported by Icon Component.
+ **/
+export enum Size {
+  small = 14,
+  medium = 18,
+  large = 32,
+  xLarge = 40,
 }
 
-export type { IconProps, Size };
+export interface IconProps {
+  /**
+   * Name of the SVG icon that is available in the theme.
+   **/
+  name: Image;
+
+  /**
+   * Different sizes supported by the Icon Component.
+   **/
+  size?: keyof typeof Size;
+
+  /**
+   * Colors that are available in the theme.
+   **/
+  color?: Color;
+}
--- src/components/primitive/Icon-svg/icon-svg.component.tsx
+++ src/components/primitive/Icon-svg/icon-svg.component.tsx
@@ -1,7 +1,8 @@
-import { IconProps, sizes } from './icon-svg.component.props';
+import React from 'react';
 import { ReactSVG } from 'react-svg';
-import { SvgWrapper } from './icon-svg.component.style';
 import { withTheme } from 'styled-components';
+import { IconProps, Size } from './icon-svg.component.props';
+import { SvgWrapper } from './icon-svg.component.style';
 import { IStyledTheme } from '../../../themes/styled-components';
 
 export const IconSvg: React.FunctionComponent<IconProps> = withTheme((props: IconProps & IStyledTheme) => {
@@ -17,8 +18,8 @@ export const IconSvg: React.FunctionComponent<IconProps> = withTheme((props: Ico
       <ReactSVG
         src={images[name]}
         beforeInjection={(svg) => {
-          svg.setAttribute('width', `${sizes[size]}px`);
-          svg.setAttribute('height', `${sizes[size]}px`);
+          svg.setAttribute('width', `${Size[size]}px`);
+          svg.setAttribute('height', `${Size[size]}px`);
         }}
       />
     </SvgWrapper>
