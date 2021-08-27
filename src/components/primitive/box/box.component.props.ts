8fcabf1144dc300ef8907759141b064c391ea072
Chanchal Sadhukhan <55362491+cksadhukhan@users.noreply.github.com>
2021-08-27 23:43:08 +0530

    #9 - Create a box component (#22)
    * adds box component
    
    * adds lodash package
    
    * updates box component
    
    * updates box.component.tsx
    
    * updates index.ts in components
    
    * updates box component props
    
    * updates box component and box component props

---
+++ src/components/primitive/box/box.component.props.ts
@@ -0,0 +1,170 @@
+import { ColProps, RowProps } from 'react-grid-system';
+import { Color } from '../../../themes';
+import { SpacingTypes } from '../../../utils';
+
+export interface BoxComponentProps extends ColProps, RowProps {
+  /**
+    flex direction for the box
+   */
+  row?: boolean;
+
+  /**
+    Border radius facilitates curved borders
+    */
+  borderRadius?: number;
+
+  /**
+    Children components.
+    */
+  children?: React.ReactNode;
+
+  /**
+    Background color for the box. Useful in dev environment.
+    */
+  color?: Color;
+
+  /**
+    Horizontally center align child components
+    */
+  hCenter?: boolean;
+
+  /**
+    Bottom align child components
+    */
+  bottom?: boolean;
+
+  /**
+    Vertically center align child components
+    */
+  vCenter?: boolean;
+
+  /**
+    Height of the container
+    */
+  height?: number;
+
+  /**
+    Width of the container
+    */
+  width?: number;
+
+  /**
+    Maximum height of the container
+    */
+  maxHeight?: number;
+
+  /**
+    Maximum width of the container
+    */
+  maxWidth?: number;
+
+  /**
+    Flex size
+    */
+  flex?: number;
+
+  /**
+    Flex wrap
+    */
+  wrap?: boolean;
+
+  /**
+    Hides the view when set to true
+    */
+  hidden?: boolean;
+
+  /**
+    Component margin top
+    */
+  marginTop?: SpacingTypes | number;
+
+  /**
+    Component margin bottom
+    */
+  marginBottom?: SpacingTypes | number;
+
+  /**
+    Component vertical margin
+    */
+  marginVertical?: SpacingTypes | number;
+
+  /**
+    Component horizontal margin
+    */
+  marginHorizontal?: SpacingTypes | number;
+
+  /**
+    Component right margin
+    */
+  marginRight?: SpacingTypes | number;
+
+  /**
+    Component left margin
+    */
+  marginLeft?: SpacingTypes | number;
+
+  /**
+    Component margin
+    */
+  margin?: SpacingTypes | number;
+
+  /**
+    Component padding
+    */
+  padding?: SpacingTypes | number;
+
+  /**
+    Component padding top
+    */
+  paddingTop?: SpacingTypes | number;
+
+  /**
+      Component padding bottom
+      */
+  paddingBottom?: SpacingTypes | number;
+
+  /**
+      Component vertical padding
+      */
+  paddingVertical?: SpacingTypes | number;
+
+  /**
+      Component horizontal padding
+      */
+  paddingHorizontal?: SpacingTypes | number;
+
+  /**
+      Component right padding
+      */
+  paddingRight?: SpacingTypes | number;
+
+  /**
+      Component left padding
+      */
+  paddingLeft?: SpacingTypes | number;
+
+  /**
+    Aligns content to right
+    */
+  rightAlign?: boolean;
+
+  /**
+    Aligns content to center
+    */
+  centerAlign?: boolean;
+
+  /**
+    Opacity pf the grid
+    */
+  opacity?: number;
+
+  /**
+    An optional style override useful for padding & margin.
+    */
+  style?: React.CSSProperties;
+
+  /**
+    justify content space-between for the box
+   */
+  spaceBetween?: boolean;
+}
---
+++ src/components/primitive/box/box.component.tsx
@@ -0,0 +1,156 @@
+import React from 'react';
+import { Col, Row } from 'react-grid-system';
+import styled, { withTheme } from 'styled-components';
+import _ from 'lodash';
+import { BoxComponentProps } from './box.component.props';
+import { spacing } from '../../../utils';
+import { IStyledTheme } from '../../../themes/styled-components';
+
+// @ts-ignore
+export const Box: React.FunctionComponent<BoxComponentProps> = withTheme(
+  (props: BoxComponentProps & IStyledTheme) => {
+    const {
+      theme: { colors },
+      row,
+      borderRadius,
+      spaceBetween,
+      flex,
+      hidden,
+      bottom,
+      children,
+      color,
+      height,
+      width,
+      maxHeight,
+      maxWidth,
+      hCenter,
+      vCenter,
+      marginTop,
+      marginBottom,
+      marginLeft,
+      marginRight,
+      marginHorizontal,
+      marginVertical,
+      margin,
+      paddingTop,
+      paddingBottom,
+      paddingLeft,
+      paddingRight,
+      paddingHorizontal,
+      paddingVertical,
+      padding,
+      wrap,
+      rightAlign,
+      centerAlign,
+      style: styleOverride,
+      ...rest
+    } = props;
+
+    // padding values in the four directions
+    // the priority is in order as they are declared
+    const paddingTopValue = paddingTop ?? paddingVertical ?? padding ?? 0;
+    const paddingBottomValue = paddingBottom ?? paddingVertical ?? padding ?? 0;
+    const paddingLeftValue = paddingLeft ?? paddingHorizontal ?? padding ?? 0;
+    const paddingRightValue = paddingRight ?? paddingHorizontal ?? padding ?? 0;
+
+    // padding values in the four directions
+    // the priority is in order as they are declared
+    const marginTopValue = marginTop ?? marginVertical ?? margin ?? 0;
+    const marginBottomValue = marginBottom ?? marginVertical ?? margin ?? 0;
+    const marginLeftValue = marginLeft ?? marginHorizontal ?? margin ?? 0;
+    const marginRightValue = marginRight ?? marginHorizontal ?? margin ?? 0;
+
+    //max-width
+    const _maxWidth = maxWidth ?? width;
+
+    // bottom align and right align cssProperties based on the flex direction
+    const _bottomAlign = row
+      ? { alignItems: 'flex-end' }
+      : { justifyContent: 'flex-end' };
+    const _rightAlign = row
+      ? { justifyContent: 'flex-end' }
+      : { alignItems: 'flex-end' };
+
+    const style = {
+      display: 'flex',
+      flexDirection: row ? 'row' : 'column',
+      minHeight: 'auto',
+      ...(!_.isNil(flex) ? { flex: flex } : {}),
+      ...(!_.isNil(borderRadius) ? { borderRadius: `${borderRadius}rem` } : {}),
+      ...(!_.isNil(color) ? { backgroundColor: colors[color] } : {}),
+      ...(!_.isNil(hCenter) ? { alignItems: 'center' } : {}),
+      ...(!_.isNil(vCenter) ? { justifyContent: 'center' } : {}),
+      ...(!_.isNil(height) ? { height: `${height}rem` } : {}),
+      ...(!_.isNil(width) ? { width: `${width}rem` } : {}),
+      ...(!_.isNil(maxWidth) ? { maxWidth: `${_maxWidth}rem` } : {}),
+      ...(!_.isNil(maxHeight) ? { maxHeight: `${maxHeight}rem` } : {}),
+      ...(!_.isNil(wrap) ? { flexWrap: 'wrap' } : {}),
+
+      ...{
+        marginTop: _.isNumber(marginTopValue)
+          ? `${marginTopValue}rem`
+          : `${spacing[marginTopValue]}rem`,
+      }, //margin top
+      ...{
+        marginBottom: _.isNumber(marginBottomValue)
+          ? `${marginBottomValue}rem`
+          : `${spacing[marginBottomValue]}rem`,
+      }, //margin bottom
+      ...{
+        marginLeft: _.isNumber(marginLeftValue)
+          ? `${marginLeftValue}rem`
+          : `${spacing[marginLeftValue]}rem`,
+      }, //margin left
+      ...{
+        marginRight: _.isNumber(marginRightValue)
+          ? `${marginRightValue}rem`
+          : `${spacing[marginRightValue]}rem`,
+      }, //margin right
+
+      ...{
+        paddingTop: _.isNumber(paddingTopValue)
+          ? `${paddingTopValue}rem`
+          : `${spacing[paddingTopValue]}rem`,
+      }, //padding top
+      ...{
+        paddingBottom: _.isNumber(paddingBottomValue)
+          ? `${paddingBottomValue}rem`
+          : `${spacing[paddingBottomValue]}rem`,
+      }, //padding bottom
+      ...{
+        paddingLeft: _.isNumber(paddingLeftValue)
+          ? `${paddingLeftValue}rem`
+          : `${spacing[paddingLeftValue]}rem`,
+      }, //padding left
+      ...{
+        paddingRight: _.isNumber(paddingRightValue)
+          ? `${paddingRightValue}rem`
+          : `${spacing[paddingRightValue]}rem`,
+      }, //padding right
+
+      ...(!!bottom ? _bottomAlign : {}),
+      ...(!!rightAlign ? _rightAlign : {}),
+      ...(!!centerAlign
+        ? { justifyContent: 'center', alignItems: 'center' }
+        : {}),
+
+      ...(!!spaceBetween ? { justifyContent: 'space-between' } : {}),
+      ...styleOverride,
+    };
+
+    const Layout = row ? Row : Col;
+
+    const Base = styled(Layout)<BoxComponentProps>``;
+
+    if (hidden) {
+      return <></>;
+    }
+
+    return (
+      // @ts-ignore
+      <Base style={style} {...rest}>
+        {children}
+      </Base>
+    );
+  }
+);
