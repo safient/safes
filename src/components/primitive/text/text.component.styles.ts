00c4a84d827aaefd5d77a075b9d46198a9839d9b
koushith <koushith97@gmail.com>
2021-09-01 21:34:56 +0530

    refactors text component

--- src/components/primitive/text/text.component.styles.ts
+++ src/components/primitive/text/text.component.styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { FontSize, FontWeight, TextComponentProps, Variant } from './text.component.props';
+import { FontSize, FontWeight, TextComponentProps } from './text.component.props';
 
 /**
  * Base style for text component that can be extended.
@@ -44,47 +44,4 @@ const Small = styled(Base)<TextComponentProps>`
   font-weight: ${FontWeight.semiBold};
 `;
 
-/**
- * returns the Text style based on the variant passed by the user.
- */
-const getVariant = (variant: keyof typeof Variant | undefined) => {
-  let TextComponent;
-  switch (variant) {
-    case Variant.title:
-      TextComponent = Title;
-      break;
-    case Variant.contentHeader:
-      TextComponent = ContentHeader;
-      break;
-    case Variant.content:
-      TextComponent = Content;
-      break;
-    case Variant.small:
-      TextComponent = Small;
-      break;
-    default:
-      TextComponent = Base;
-  }
-
-  const Typography = styled(TextComponent)<TextComponentProps>`
-    /* custom styles goes here */
-    text-transform: ${({ upperCase }) => upperCase && `uppercase`};
-    text-transform: ${({ lowerCase }) => lowerCase && `lowercase`};
-    text-transform: ${({ capitalize }) => capitalize && `lowercase`};
-    text-align: ${({ center }) => center && 'center'};
-    text-align: ${({ left }) => left && `left`};
-    text-align: ${({ right }) => right && `right`};
-    line-height: ${({ lineHeight }) => lineHeight && `${lineHeight}em`};
-    padding-left: ${({ paddingLeft }) => paddingLeft && ` ${paddingLeft}rem`};
-    padding-right: ${({ paddingRight }) => paddingRight && ` ${paddingRight}rem`};
-    font-weight: ${({ noBold }) => noBold && `normal`};
-    font-weight: ${({ bold500 }) => bold500 && `500`};
-    font-weight: ${({ bold600 }) => bold600 && `600`};
-    font-size: ${({ size }) => size && `${FontSize[size]}rem`};
-    font-size: ${({ size }) => size && `${size}rem`};
-    color: ${({ color, theme: { colors } }) => color && colors[color]};
-  `;
-  return Typography;
-};
-
-export { Base, Title, ContentHeader, Content, Small, getVariant };
+export { Base, Title, ContentHeader, Content, Small };
--- src/components/primitive/text/text.component.tsx
+++ src/components/primitive/text/text.component.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { withTheme } from 'styled-components';
-import { getVariant } from './text.component.styles';
-import { TextComponentProps } from './text.component.props';
+import styled, { withTheme } from 'styled-components';
+import { Title, ContentHeader, Content, Small, Base } from './text.component.styles';
+import { TextComponentProps, Variant, FontSize } from './text.component.props';
 import { translate } from '../../../i18n/translate';
 import { IStyledTheme } from '../../../themes/styled-components';
 
@@ -10,7 +10,47 @@ export const Text: React.FunctionComponent<TextComponentProps> = withTheme(
     const { text, tx, txOptions, variant, children, ...rest } = props;
     const i18nText = tx && translate(tx, txOptions);
     const content = i18nText || text || children;
-    const Typography = getVariant(variant);
+
+    const getVariant = (variant: Variant = Variant.content) => {
+      let TextComponent;
+      switch (variant) {
+        case Variant.title:
+          TextComponent = Title;
+          break;
+        case Variant.contentHeader:
+          TextComponent = ContentHeader;
+          break;
+        case Variant.content:
+          TextComponent = Content;
+          break;
+        case Variant.small:
+          TextComponent = Small;
+          break;
+        default:
+          TextComponent = Base;
+      }
+      return TextComponent;
+    };
+
+    const StyledText = getVariant(variant ? Variant[variant] : undefined);
+    const Typography = styled(StyledText)<TextComponentProps>`
+      /* custom styles goes here */
+      text-transform: ${({ upperCase }) => upperCase && `uppercase`};
+      text-transform: ${({ lowerCase }) => lowerCase && `lowercase`};
+      text-transform: ${({ capitalize }) => capitalize && `lowercase`};
+      text-align: ${({ center }) => center && 'center'};
+      text-align: ${({ left }) => left && `left`};
+      text-align: ${({ right }) => right && `right`};
+      line-height: ${({ lineHeight }) => lineHeight && `${lineHeight}em`};
+      padding-left: ${({ paddingLeft }) => paddingLeft && ` ${paddingLeft}rem`};
+      padding-right: ${({ paddingRight }) => paddingRight && ` ${paddingRight}rem`};
+      font-weight: ${({ noBold }) => noBold && `normal`};
+      font-weight: ${({ bold500 }) => bold500 && `500`};
+      font-weight: ${({ bold600 }) => bold600 && `600`};
+      font-size: ${({ size }) => size && `${FontSize[size]} rem`};
+      font-size: ${({ size }) => size && `${size}rem`};
+      color: ${({ color, theme: { colors } }) => color && colors[color]};
+    `;
 
     return <Typography {...rest}>{content}</Typography>;
   }
