8dd6c87c56f09161ab2bd16060f340d9b877d5b7
koushith <koushith97@gmail.com>
2021-09-28 01:27:32 +0530

    cleared unused comments

--- src/utils/spacing.ts
+++ src/utils/spacing.ts
@@ -1,38 +1,6 @@
-// export const SPACES = {
-//   0: '0rem',
-//   2: '0.2rem',
-//   4: '0.4rem',
-//   6: '0.6rem',
-//   8: '0.8rem',
-//   10: '1rem',
-//   12: '1.2rem',
-//   14: '1.4rem',
-//   16: '1.6rem',
-//   18: '1.8rem',
-//   20: '2.0rem',
-//   22: '2.2rem',
-//   24: '2.4rem',
-//   26: '2.6rem',
-//   28: '2.8rem',
-//   30: '3.0rem',
-//   32: '3.2rem',
-//   34: '3.4rem',
-//   36: '3.6rem',
-// };
-
 /**
- * Spacing must be consistent across the app. These values must be used instead of hard coding.
+ *  Common Spacing values used throught the application.
  */
-// export const spacing = {
-//   tiny: 4,
-//   small: 8,
-//   medium: 12,
-//   large: 16,
-//   xLarge: 24,
-//   xxLarge: 36,
-//   xxxLarge: 48,
-//   huge: 64,
-// };
 
 export const spacing = {
   tiniest: 0.4,
