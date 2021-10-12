050d764cb59402fe4afbf668965cd100b001f348
koushith <koushith97@gmail.com>
2021-10-12 16:07:36 +0530

    adds creat-card component

--- src/screens/home/home.screen.tsx
+++ src/screens/home/home.screen.tsx
@@ -1,18 +1,10 @@
 import React from 'react';
 import { Text } from '../../components/primitive';
-import styled from 'styled-components';
-import { NavBar } from '../../components/primitive/navbar/navbar.component';
-import { BottomTab } from '../../components/primitive/bottom-tab/bottom-tab.component';
 
-const HomeContainer = styled.section`
-  padding: 10px;
-  margin: 10px;
-`;
 export const HomeScreen: React.FC = () => {
   return (
-    <div>
-      <NavBar />
-      {/* <BottomTab /> */}
-    </div>
+    <>
+      <Text variant='title'>Dashboard</Text>
+    </>
   );
 };
