f2c07112ca72810acf7a5bdfbb3ea806c9ebc5d8
Chanchal Sadhukhan <cksadhukhan@gmail.com>
2021-11-09 20:50:25 +0530

    modifies enum type

--- src/services/storage/storage.service.impl.ts
+++ src/services/storage/storage.service.impl.ts
@@ -6,17 +6,17 @@ export enum StorageKey{
 }
 
 export class StorageServiceImpl implements StorageService {
-  async get(key: keyof typeof StorageKey): Promise<any> {
+  async get(key: StorageKey): Promise<any> {
     const data = await localStorage.getItem(key);
     return data ? JSON.parse(data) : undefined;
   }
 
-  async set(key: keyof typeof StorageKey, value: any) {
+  async set(key: StorageKey, value: any) {
     const data = JSON.stringify(value);
     await localStorage.setItem(key, data);
   }
 
-  async remove(key: keyof typeof StorageKey) {
+  async remove(key: StorageKey) {
     await localStorage.removeItem(key);
   }
 }
--- src/services/storage/storage.service.ts
+++ src/services/storage/storage.service.ts
@@ -6,17 +6,17 @@ export interface StorageService {
    * @param key
    * @param value
    */
-  set(key: keyof typeof StorageKey, value: any): void;
+  set(key: StorageKey, value: any): void;
 
   /**
    * For getting value from localStorage
    * @param key
    */
-  get(key: keyof typeof StorageKey): any;
+  get(key: StorageKey): any;
 
   /**
    * For removing value from localStorage
    * @param key
    */
-  remove(key: keyof typeof StorageKey): void;
+  remove(key: StorageKey): void;
 }
