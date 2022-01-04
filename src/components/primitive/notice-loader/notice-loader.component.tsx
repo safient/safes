8f25ce308e251506dea6023955bc0181155e0394
koushith <koushith97@gmail.com>
2022-01-04 15:20:08 +0530

    adds icon support

--- src/components/primitive/notice-loader/notice-loader.component.tsx
+++ src/components/primitive/notice-loader/notice-loader.component.tsx
@@ -5,15 +5,16 @@ import { LoaderComponentProps } from './notice-loader.component.props';
 import { Spinner, LoaderText, HelperText, NoticeLoaderContainer } from './notice-loader.component.styles';
 
 export const NoticeLoader: React.FC<LoaderComponentProps> = (props) => {
-  const { label, spinner, helperText } = props;
+  const { label, spinner, helperText, IconName } = props;
   const { images } = useTheme();
+  const url = IconName ? images[IconName] : images.lockLoader;
 
   return (
     <GenericModal show={true} hide noClose>
       <NoticeLoaderContainer>
-        {spinner ? <Spinner /> : <Image src={images.lockLoader} width={7} />}
+        {spinner ? <Spinner /> : <Image src={url} width={7} />}
         <LoaderText variant='small' {...label} />
-        <HelperText variant='small' text={helperText} />
+        <HelperText variant='small' {...helperText} />
       </NoticeLoaderContainer>
     </GenericModal>
   );
