937f0a199c33969207187e5dc366c28c9907f59e
Koshik Raj <koshik.raj@gmail.com>
2022-01-29 09:35:14 +0530

    updates alert icons and text colors

--- src/themes/palette.ts
+++ src/themes/palette.ts
@@ -11,21 +11,21 @@ export const palette = {
   royalBlueLighter: '#6366f1', // Toggle Switch
   royalBlueTransparent: 'rgba(84, 82, 246, 0.1)', // NavLink active color
 
-  blueLight: '#014361', // info alert text
+  blueLight: '#46A1CB', // info alert text
   blueLighter: '#e5f6fd', // info alert bg
 
   green: '#2ba745',
-  greenLight: '#1E4620', //green text color
+  greenLight: '#49B393', //green text color
   greenLighter: '#edf7ed', //alert bg
   greenLightest: '#badbcc', //for border
   greenTransparent: 'rgba(73, 179, 147, 0.1)',
   greenDark: '#065f43', //for icons
   yellow: '#ffc107', //default yellow color
-  yellowLight: '#663c00', //for text
+  yellowLight: '#CB974E', //for text
   yellowLighter: '#fff4e5', //alert bg
   yellowLightest: '#ffecb5', //border
   red: '#dc3545',
-  redLight: '#5f2120', //text color
+  redLight: '#D55555', //text color
   redLighter: '#fdeded', // alert bg
   redLightest: '#f5c2c7', //borders
   redTransparent: 'rgba(255, 162, 136, 0.1)',
