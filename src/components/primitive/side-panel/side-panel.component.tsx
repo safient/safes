2ddc4975776b630370655ab0c70db71aa0b3d541
Chanchal Sadhukhan <cksadhukhan@gmail.com>
2022-02-01 22:27:27 +0530

    updates side panel

--- src/components/primitive/side-panel/side-panel.component.tsx
+++ src/components/primitive/side-panel/side-panel.component.tsx
@@ -1,70 +1,52 @@
 import React, { useEffect, useRef } from 'react';
 import { styled } from 'themes';
 import { SidePanelComponentProps } from './side-panel.component.props';
+import { Card } from '../card/card.component';
 
-const Container = styled.div`
+const PanelContainer = styled.div`
   height: 100% !important;
   display: flex;
   flex-direction: column;
   border-right: 1px solid;
   border-radius: 0;
   z-index: 999;
-  background-color: red;
+  background-color: ${({ theme: { colors } }) => colors.white};
   transition: 0.8s ease;
-`;
-
-const OpenButton = styled.div`
-  height: 50px !important;
-  border-top-left-radius: 10rem;
-  border-bottom-left-radius: 9rem;
-  width: 20px !important;
   position: absolute;
   right: 0;
-  top: 4rem;
-  outline: none;
-  z-index: 1;
-  background-color: rgba(64, 194, 133, 0.693);
-  border-color: rgba(64, 194, 133, 0.693);
-  border-left: 0;
-  cursor: pointer;
+  top: 0;
+  bottom: 0;
+  width: 30vw;
+
+  @media (max-width: 768px) {
+    width: 100vw;
+  }
 `;
 
-const CloseButton = styled.div`
-  height: 50px;
-  border-top-left-radius: 10rem;
-  border-bottom-left-radius: 9rem;
-  width: 20px;
-  position: absolute;
-  right: 25vw;
-  top: 0rem;
-  outline: none;
-  z-index: 9999;
-  background-color: rgba(64, 194, 133, 0.693);
-  border-color: rgba(64, 194, 133, 0.693);
-  border-left: 0;
-  cursor: pointer;
+const ContentContainer = styled(Card)`
+  padding: 3rem !important;
 `;
 
 export const SidePanel: React.FC<SidePanelComponentProps> = (
   props: SidePanelComponentProps
 ) => {
-  const { innerWidth: vw, innerHeight: vh } = window;
   const { show, onClose, togglePanel, children } = props;
 
-  const wrapperRef = useRef<any>(null);
+  const wrapperRef = useRef<HTMLDivElement>(null);
 
   const closePanel = () => {
     onClose();
   };
 
-  const onEscapeKeydown = (event: any) => {
-    if (event.keyCode === 27) {
+  const onEscapeKeydown = (event: KeyboardEvent) => {
+    if (event.key === 'Escape') {
       closePanel();
     }
   };
 
   React.useEffect(() => {
     document.addEventListener('keydown', onEscapeKeydown, false);
+
     return () => {
       document.removeEventListener('keydown', onEscapeKeydown, false);
     };
@@ -72,7 +54,6 @@ export const SidePanel: React.FC<SidePanelComponentProps> = (
 
   useEffect(() => {
     function handleClickOutside(event: any) {
-      // @ts-ignore
       if (wrapperRef.current && !wrapperRef.current?.contains(event.target)) {
         closePanel();
       }
@@ -80,6 +61,7 @@ export const SidePanel: React.FC<SidePanelComponentProps> = (
 
     // Bind the event listener
     document.addEventListener('mousedown', handleClickOutside);
+
     return () => {
       // Unbind the event listener on clean up
       document.removeEventListener('mousedown', handleClickOutside);
@@ -87,29 +69,14 @@ export const SidePanel: React.FC<SidePanelComponentProps> = (
   }, [wrapperRef]);
 
   if (!show) {
-    return (
-      <>
-        <OpenButton onClick={togglePanel} className="toggle-menu" />
-      </>
-    );
+    return <></>;
   }
 
   return (
     <React.Fragment>
-      <Container
-        ref={wrapperRef}
-        style={{
-          position: 'absolute',
-          right: 0,
-          top: 0,
-          bottom: 0,
-          width: 0.25 * vw,
-          minHeight: vh,
-        }}
-      >
-        <div className="content">{children}</div>
-      </Container>
-      <CloseButton onClick={closePanel} className="toggle-menu2" />
+      <PanelContainer ref={wrapperRef}>
+        <ContentContainer>{children}</ContentContainer>
+      </PanelContainer>
     </React.Fragment>
   );
 };
